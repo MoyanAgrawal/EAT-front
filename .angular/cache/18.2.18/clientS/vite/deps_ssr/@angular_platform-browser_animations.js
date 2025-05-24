9fedb5bdac703afad690a5da4b6a433b94d1acc4
Moyan Agrawal <moyan.agrawal@mphasis.com>
2025-05-24 12:07:26 +0530

    final commit

---
+++ .angular/cache/18.2.18/clientS/vite/deps_ssr/@angular_platform-browser_animations.js
@@ -0,0 +1,26 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  BrowserAnimationsModule,
+  InjectableAnimationEngine,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations
+} from "./chunk-IFVW2UHC.js";
+import "./chunk-YEK5VPQ5.js";
+import "./chunk-5U34YNWY.js";
+import "./chunk-4JT7VILW.js";
+import "./chunk-CIB77MNN.js";
+import {
+  ANIMATION_MODULE_TYPE
+} from "./chunk-EIEGMLYI.js";
+import "./chunk-U66U6LJ5.js";
+import "./chunk-NQ4HTGF6.js";
+export {
+  ANIMATION_MODULE_TYPE,
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations,
+  InjectableAnimationEngine as ɵInjectableAnimationEngine
+};
+//# sourceMappingURL=@angular_platform-browser_animations.js.map
